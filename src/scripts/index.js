ee211531f6ec72ca913a582845dd48fb5a58df1a
AlexandriaBlade <139504913+AlexandriaBlade@users.noreply.github.com>
2024-06-02 17:25:15 +0300

    up
    up

---
+++ src/scripts/index.js
@@ -0,0 +1,81 @@
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+
+const container = document.querySelector(".content");
+const cardsContainer = container.querySelector(".places__list");
+const cardData = initialCards;
+
+// @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
+  return cardElement;
+}
+
+// @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, deleteCard);
+  cardsContainer.appendChild(cardElement);
+});
+
+// Импорт стилей и функций
+import "../pages/index.css";
+import { initialCards } from "./cards.js";
+import { likeCard } from "./card.js";
+// Импорт функций модальных окон
+import {  closePopup,  openPopup} from "./modal.js";
+
+// DOM узлы
+const profileEditButton = document.querySelector(".profile__edit-button");
+const newCardButton = document.querySelector(".profile__add-button");
+const placesList = document.querySelector(".places__list");
+
+// Вывод карточек на страницу
+initialCards.forEach((element) => {
+  placesList.append(createCard(element, deleteCard, likeCard));
+});
+
+// Обработчики событий для кнопок редактирования профиля и добавления новой карточки
+// Находим элементы один раз и сохраняем ссылки на них
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+// Обработчики событий для кнопок редактирования профиля и добавления новой карточки
+profileEditButton.addEventListener("click", () => {
+  openPopup(popupEdit);
+  const nameInput = popupEdit.querySelector('input[name="name"]');
+  const descriptionInput = popupEdit.querySelector('input[name="description"]');
+
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+});
+
+newCardButton.addEventListener("click", () => {
+  openPopup(popupNewCard);
+});
+
+// Установка обработчиков событий для закрытия попапов
+document.querySelectorAll(".popup").forEach((popup) => {
+  closePopup(popup);
+});
+
+
+
+
+
+
+
---
+++ src/scripts/modal.js
@@ -0,0 +1,29 @@
+function openPopup(popup) {
+    popup.classList.add('popup_opened'); // Объединенный класс для анимации и отображения
+    document.addEventListener("keyup", handleEscUp);
+};
+
+function closePopup(popup) {
+    popup.classList.remove("popup_opened");
+    document.removeEventListener("keyup", handleEscUp);
+};
+
+const handleEscUp = (evt) => {
+    if (evt.key === "Escape") {
+        closePopup(document.querySelector(".popup_opened"));
+    }
+};
+
+function setClosePopupEventListeners() {
+    document.addEventListener("click", closePopupEvent);
+}
+
+function closePopupEvent(evt) {
+    if (evt.target.classList.contains("popup__close") || evt.target.classList.contains("popup")) {
+        const popup = evt.target.closest('.popup');
+        closePopup(popup);
+    }
+}
+
+
+export { openPopup, closePopup, setClosePopupEventListeners };
