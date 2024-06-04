7874f7288b795b20d6794431456441177f1a12f9
AlexandriaBlade <139504913+AlexandriaBlade@users.noreply.github.com>
2024-06-04 22:16:16 +0300

    fixed
    fixed

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,29 +1,18 @@
-function openPopup(popup) {
-    popup.classList.add('popup_is-opened'); // Объединенный класс для анимации и отображения
-    document.addEventListener("keyup", handleEscUp);
-};
-
-function closePopup(popup) {
-    popup.classList.remove("popup_is-opened");
-    document.removeEventListener("keyup", handleEscUp);
-};
-
-const handleEscUp = (evt) => {
-    if (evt.key === "Escape") {
-        closePopup(document.querySelector(".popup_is-opened"));
-    }
-};
-
-function setClosePopupEventListeners() {
-    document.addEventListener("click", closePopupEvent);
+function openPopup (popup) {
+  popup.classList.add('popup_is-opened');
+  document.addEventListener('keydown', handleEscape); 
 }
 
-function closePopupEvent(evt) {
-    if (evt.target.classList.contains("popup__close") || evt.target.classList.contains("popup")) {
-        const popup = evt.target.closest('.popup');
-        closePopup(popup);
-    }
+function closePopup (popup) {
+  popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', handleEscape);
 }
 
+function handleEscape(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_is-opened');
+    closePopup(openedPopup);
+  }
+}
 
-export { openPopup, closePopup, setClosePopupEventListeners };
+  export {openPopup, closePopup}
