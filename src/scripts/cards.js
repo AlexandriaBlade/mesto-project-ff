9dac5e22f6a105a565bf8abd4521b96064095cfc
AlexandriaBlade <139504913+AlexandriaBlade@users.noreply.github.com>
2024-06-08 02:41:18 +0300

    up

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,30 +1,43 @@
 const cardTemplate = document.querySelector('#card-template');
 
-function createCard(name, link, deleteCard, handleImageClick, like) {
+function createCard(item, userId, name, link, handleImageClick, handleDeleteButtonClick, handleLikeButton) {
   const listItemCopy = cardTemplate.content.cloneNode(true);
-  const deleteButton = listItemCopy.querySelector('.card__delete-button');
   const cardImage = listItemCopy.querySelector('.card__image');
-  const likeButton = listItemCopy.querySelector('.card__like-button');
   const cardTitle = listItemCopy.querySelector('.card__title');
+  const deleteButton = listItemCopy.querySelector('.card__delete-button')
+  const likeButton = listItemCopy.querySelector('.card__like-button');
 
   cardTitle.textContent = name;
   cardImage.src = link;
   cardImage.alt = name;
 
-  deleteButton.addEventListener('click', deleteCard);
-  cardImage.addEventListener('click', handleImageClick);
-  likeButton.addEventListener('click', like);
-  
-  return listItemCopy
-}
+  if(!(item.owner._id === userId)) {
+    deleteButton.setAttribute('style', 'display: none');
+  }
 
-function deleteCard (evt) {
-  const card = evt.target.closest('.card');
-  card.remove();
+  if(item.likes.length > 0) {
+    likeButton.dataset.after = item.likes.length;
+  }
+
+  if(isLiked(item, userId)) {
+    likeButton.classList.add('card__like-button_is-active')
+  }
+
+  cardImage.addEventListener('click', handleImageClick);
+  deleteButton.addEventListener('click', (evt) => handleDeleteButtonClick(item, evt));
+  likeButton.addEventListener('click', (evt) => handleLikeButton(item._id, evt, likeButton));
+
+  return listItemCopy
 }
 
 function like (evt) {
   evt.target.classList.toggle('card__like-button_is-active');
 }
 
-export {createCard, deleteCard, like}
+function isLiked (item, userId) {
+  return item.likes.some((element) => {
+   return element._id === userId
+  })
+ }
+
+export {createCard, like}
